be479b5a554dec3ea85da8240e4e00439dc2ba20
Jonathan Haber <jryanhaber@gmail.com>
2017-05-19 15:31:09 -0700

    input form styled

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,6 +1,6 @@
 import React ,{ Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common'
+import { View } from 'react-native';
+import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
 state = { text: ''};
@@ -9,10 +9,11 @@ state = { text: ''};
 		return (
 			<Card>
 				<CardSection> 
-					<TextInput
-						value={this.state.text}
+					<Input
+						value={ this.state.text }
 						onChangeText = {text => this.setState({ text })}
-						style={{height:20, width:'100%'}}/>
+						label="Email"
+						/>
 				</CardSection> 
 				<CardSection/> 
 				<CardSection>
--- components/common/index.js
+++ components/common/index.js
@@ -2,5 +2,6 @@ export * from './Button';
 export * from './Card';
 export * from './CardSection';
 export * from './CustomHeader';
+export * from './Input';
 
  
